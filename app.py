c758246d5440e0dd5a4a4d2bd24851ecb7aa81cf
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2026-09-15 23:42:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from pathlib import Path
 import time
-.........................
+........................
 # Simple Page Confi
 st.set_page_config(page_title="SAMRI DXF", layout="wide")
 
