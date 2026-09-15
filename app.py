807007fc725bf0dd71cc9b3d7508ef87ad49c7cb
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2026-09-15 23:47:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pathlib import Path
 import time
 
-# Simple Page Con
+# Simple Page Co
 st.set_page_config(page_title="SAMRI DXF", layout="wide")
 
 # This must be at the top to ensure the UI renders immediately
